1b2b3dd0bb97267e4ef078993ca274b52bf7d8d3
luigi1015 <github@314ghz.com>
2014-01-03 13:37:32 -0600

    Added a little to the header files.

--- Link.h
+++ Link.h
@@ -2,7 +2,6 @@
 #define Link_h
 
 #include <string>
-#include <vector>
 #include "Node.h"
 
 namespace BasicNetwork
@@ -14,6 +13,10 @@ namespace BasicNetwork
 			Node *node1;//One of the nodes if undirected. The beginning node if directed.
 			Node *node2;//The other node if undirected. The end node if directed.
 			bool directed;//Directed if true, undirected if false.
+			int maxCapacity;//THe maximum amount of whatever that can move through the link. For example, this could be the maximum amount of current flowing through a wire or the maximum number of cars that can move througha street per unit of time.
+			std::string name;//The name of the link, possibly the display name.
+			std::string ID;//The ID of the link, possibly a technical ID/number meant to be used internally.
+			std::string description;//A description of the link.
 
 		public:
 			void setCost( int newCost );
@@ -24,6 +27,14 @@ namespace BasicNetwork
 			int getSecondNode();
 			void setDirected( bool newDirected );//True means directed, false means undirected.
 			int getDirected();//True means directed, false means undirected.
+			void setMaxCapacity( int newMaxCapacity );
+			int getMaxCapacity();
+			void setName( std::string newName );
+			std::string getName();
+			void setID( std::string newID );
+			std::string getID();
+			void setDescription( std::string newDescription );
+			std::string getDescription();
 	}
 }
 
--- Node.h
+++ Node.h
@@ -10,8 +10,11 @@ namespace BasicNetwork
 	class Node
 	{//A node of a basic network with directed links.
 		private:
-			std::string name;//The name of the 
+			std::string name;//The name of the node, possibly the display name.
 			std::vector<Link *> neighbors;//A list of links connected to this node.
+			std::string ID;//The ID of the node, possibly a technical ID/number meant to be used internally.
+			std::string description;//A description of the link.
+			int x, y, z;//The x, y, and z location of the node.
 
 		public:
 			void setName( std::string newName );
@@ -20,6 +23,17 @@ namespace BasicNetwork
 			Link *getLink( int index );//Get link at the specified index. Index starts at 1 and goes to the number of connected links.
 			int getLinkCount();//Returns the number of links connected to this node.
 			void clearLinks();//Clears the list of links without deleting the links themselves.
+			void setID( std::string newID );
+			std::string getID();
+			void setDescription( std::string newDescription );
+			std::string getDescription();
+			void setLocation( int newX, int newY, int newZ );
+			void setX( int newX );
+			int getX();
+			void setY( int newY );
+			int getY();
+			void setZ( int newZ );
+			int getZ();
 	}
 }
 
